a5a556f8d704ced734a258bb7ebb31be9c8f41ad
Aleix3 <Aleixbotella.1@gmail.com>
2025-03-26 17:15:46 +0100

    gameplay with keyBoard (without aiming)

--- MianCharacter/scripts/JugadorScript.lua
+++ MianCharacter/scripts/JugadorScript.lua
@@ -4,7 +4,7 @@ local playerTransf
 local playerRb = nil
 local moveSpeed = 6
 local lastValidRotation = 0
-local currentSpeed = 0         
+local currentSpeed = 0
 local acceleration = 10      
 local deceleration = 8
 local forwardVector
@@ -174,7 +174,7 @@ end
 
 function updateDash(dt)
     -- Check for dash activation
-    if (Input.is_button_pressed(Input.controllercode.East) or Input.is_key_pressed(Input.keycode.M)) and dashAvailable == true then
+    if Input.get_button(Input.action.Dash) == Input.state.Down and dashAvailable == true then
         if moveDirection ~= nil then
             local impulse = Vector3.new(moveDirection.x * dashSpeed, moveDirection.y * dashSpeed, moveDirection.z * dashSpeed)
             playerRb:set_trigger(true)
@@ -259,7 +259,7 @@ function updateEntranceAnimation(dt)
 end
 
 function handleWeaponSwitch()
-    if Input.is_button_pressed(Input.controllercode.North) == true then
+    if Input.get_button(Input.action.Skill1) == Input.state.Down then
         if pressedButtonChangeWeapon == false then
             if actualweapon == 0 then
                 actualweapon = 1
@@ -302,7 +302,40 @@ function playerMovement(dt)
     local rotationDirectionX = axisX_r * math.cos(cameraAngle) - axisY_r * math.sin(cameraAngle)
     local rotationDirectionY = axisX_r * math.sin(cameraAngle) + axisY_r * math.cos(cameraAngle)
 
-    moveDirection = Vector3.new(moveDirectionX, 0, moveDirectionY)
+    local keyboard = false
+
+   
+    if Input.is_key_pressed(Input.keycode.W) or Input.is_key_pressed(Input.keycode.A) or Input.is_key_pressed(Input.keycode.S) or Input.is_key_pressed(Input.keycode.D) then
+        if Input.is_key_pressed(Input.keycode.W) then
+            moveDirectionY = -1  
+            
+        elseif Input.is_key_pressed(Input.keycode.S) then
+            moveDirectionY = 1  
+        else
+            moveDirectionY = 0
+        end
+        
+        if Input.is_key_pressed(Input.keycode.A) then
+            moveDirectionX = -1  
+        elseif Input.is_key_pressed(Input.keycode.D) then
+            moveDirectionX = 1 
+        else
+            moveDirectionX = 0
+        end
+
+        keyboard = true
+        
+    end
+
+    if keyboard then
+        moveDirection = Vector3.new(
+        moveDirectionX * math.cos(cameraAngle) - moveDirectionY * math.sin(cameraAngle),
+        0,
+        moveDirectionX * math.sin(cameraAngle) + moveDirectionY * math.cos(cameraAngle)
+        )
+    else
+        moveDirection = Vector3.new(moveDirectionX, 0, moveDirectionY)
+    end
 
     rotationDirection = Vector3.new(moveDirectionX, 0, moveDirectionY)
 
--- MianCharacter/scripts/RifleManager.lua
+++ MianCharacter/scripts/RifleManager.lua
@@ -307,8 +307,8 @@ end
 
 function on_update(dt)
     if using then
-        local rightTrigger = Input.get_axis_position(Input.axiscode.RightTrigger)
-        local leftShoulder = Input.is_button_pressed(Input.controllercode.LeftShoulder)
+        local rightTrigger = Input.get_button(Input.action.Shoot)
+        local leftShoulder = Input.get_button(Input.action.Skill1)
 
         if ammo >= maxAmmo then
             if reloadTime == 0 then
@@ -325,7 +325,7 @@ function on_update(dt)
             shootCoolDown = shootCoolDown + dt
         end
 
-        if rightTrigger ~= 0 and (ammo < maxAmmo) and shootCoolDown >= shootCoolDownRifle then
+        if rightTrigger == Input.state.Down and (ammo < maxAmmo) and shootCoolDown >= shootCoolDownRifle then
         
             
             tripleShoot()
@@ -347,7 +347,7 @@ function on_update(dt)
             tripleShootTimer = tripleShootInterval
         end
 
-        if leftShoulder and cooldownDisruptorBulletTimeCounter >= cooldownDisruptorBulletTime then
+        if leftShoulder == Input.state.Down and cooldownDisruptorBulletTimeCounter >= cooldownDisruptorBulletTime then
             
             cooldownDisruptorBulletTimeCounter = 0
             disruptorShooted = true
