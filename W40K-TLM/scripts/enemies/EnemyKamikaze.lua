50c176f9c4a7800975fb1f2cb42ec0cdf9630f3d
Marti Sabaté <71668554+msabate00@users.noreply.github.com>
2025-04-21 17:54:52 +0200

    nivel 1, nivel2 y nivel3 update

--- W40K-TLM/scripts/enemies/EnemyKamikaze.lua
+++ W40K-TLM/scripts/enemies/EnemyKamikaze.lua
@@ -131,7 +131,7 @@ function on_update(dt)
     end
 
     if kamikaze.playerDetected then
-        KamikazeDetectionSFX:play()
+        kamikazeDetectionSFX:play()
         if kamikaze.key == 0 then
              
             kamikaze.playerScript.enemys_targeting = kamikaze.playerScript.enemys_targeting + 1
