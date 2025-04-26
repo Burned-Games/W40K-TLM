3b4e63bcd855e0e1f155d1c74c20b4da78f57036
Guillem Alqueza Mayench <guillemalqueza@gmail.com>
2025-04-26 18:21:50 +0200

    Level 1 doors

--- W40K-TLM/scripts/interactables/Door.lua
+++ W40K-TLM/scripts/interactables/Door.lua
@@ -3,6 +3,10 @@ local currentInteractions = 0
 local rigidBody = nil
 local isClosed = true
 
+local animator = nil
+local openAnimation = 0
+local closeAnimation = 1
+
 function on_ready()
     rigidBody = self:get_component("RigidbodyComponent").rb
     local children = self:get_children()
@@ -10,6 +14,9 @@ function on_ready()
         local childTag = child:get_component("TagComponent").tag
         if childTag:match("^Lever") then
             minimumInteractions = minimumInteractions + 1
+        end
+        if childTag:match("^Door") then
+            animator = child:get_component("AnimatorComponent")
         end
     end
 end
@@ -21,6 +28,7 @@ end
 function on_interact()
     currentInteractions = currentInteractions + 1
     if currentInteractions >= minimumInteractions and isClosed then
+        animator:set_current_animation(openAnimation)
         rigidBody:set_trigger(true)
         isClosed = false
     end
