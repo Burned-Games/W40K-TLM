02e5622ba533eebad311d93df00ef502c58333cd
Aleix3 <Aleixbotella.1@gmail.com>
2025-04-30 16:54:26 +0200

    another time

--- W40K-TLM/scripts/utils/enemy.lua
+++ W40K-TLM/scripts/utils/enemy.lua
@@ -530,6 +530,7 @@ end
 function enemy:make_damage(damage)
     
     if self.playerScript.godMode then return end
+    if self.playerScript.intangibleDash then return end
     if self.playerScript.isCovering then return end
 
     if self.playerScript.health > 0 then
