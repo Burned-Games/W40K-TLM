258ead235f0dfcb9455690436d4eb3a872796529
Marti Sabaté <71668554+msabate00@users.noreply.github.com>
2025-05-10 22:07:54 +0200

    splash screen

---
+++ W40K-TLM/scripts/misc/SplashScreenController.lua
@@ -0,0 +1,34 @@
+local timeToTransition = 5
+
+local contador = 0
+local fadeToBlackScript = nil
+local changeing = false
+
+function on_ready()
+    -- Add initialization code here
+    fadeToBlackScript = current_scene:get_entity_by_name("FadeToBlack"):get_component("ScriptComponent")
+    
+end
+
+function on_update(dt)
+    -- Add update code here
+
+    contador = contador + dt
+    if contador > timeToTransition and not changeing then
+        changeing = true
+        fadeToBlackScript:DoFade()
+    end
+
+
+    if changeing and not changeScene then
+        if fadeToBlackScript.fadeToBlackDoned then
+            changeScene = true
+            SceneManager.change_scene("scenes/mainMenu.TeaScene")
+        end
+    end
+
+end
+
+function on_exit()
+    -- Add cleanup code here
+end
