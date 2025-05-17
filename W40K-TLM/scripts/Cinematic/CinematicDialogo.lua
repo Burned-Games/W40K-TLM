beb926ee711a5a3e3582c7803edc4292002d1ae0
Linjiajie2002 <92087408+Linjiajie2002@users.noreply.github.com>
2025-05-18 01:26:01 +0200

    JJL-Intro

--- W40K-TLM/scripts/Cinematic/CinematicDialogo.lua
+++ W40K-TLM/scripts/Cinematic/CinematicDialogo.lua
@@ -1,17 +1,34 @@
-dialogLines = {
-    { name = "Carlos", text = "Hola, bienvenido al mundo" },
-    { name = "Ana", text = "Espero que estes preparado para la aventura." },
-    { name = "Carlos", text = "Vamos alla" }
-}
 
+--Audio
+local diaCine = nil
+
+--Dialogo
+local dialogLines = nil
 --dialog
 local dialogScriptComponent = nil
 
 function on_ready()
 
+    --Audio
+    --dia1_audio1 = current_scene:get_entity_by_name("dia1_audio1"):get_component("AudioSourceComponent")
+    --dia1_audio2 = current_scene:get_entity_by_name("dia1_audio2"):get_component("AudioSourceComponent")
+    -- dialogLines = {
+    --     { name = "Carlos", text = "Hola, bienvenido al mundo", audio = dia1_audio1 },
+    --      { name = "Carlos", text = "Hola, bienvenido al mundo", audio = dia1_audio2 }
+    -- }
+
+    --Dialogo
+    dialogLines = {
+        { name = "DeciusMarcellus", text = "Approaching Temperis, 1 minute until planetfall. Be ready for the landing, we detect multiple green skins lurking around, the way to Martyria Eterna won't be easy. Good luck brother, the Emperor protects."}
+    }
+
+   
+    
     dialogScriptComponent = current_scene:get_entity_by_name("DialogManager"):get_component("ScriptComponent")
-    --dialogScriptComponent.changeAutoTime(1)
     dialogScriptComponent.start_dialog(dialogLines)
+
+
+
 end
 
 function on_update(dt)
