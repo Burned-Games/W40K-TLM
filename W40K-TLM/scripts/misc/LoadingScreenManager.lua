a614b7e3b9d6722b642cee20700a6220ee31a345
Galycon <2004rasr@gmail.com>
2025-05-19 21:10:44 +0200

    New text on loading screen

--- W40K-TLM/scripts/misc/LoadingScreenManager.lua
+++ W40K-TLM/scripts/misc/LoadingScreenManager.lua
@@ -15,14 +15,47 @@ local changeing = false
 local changed = false
 
 local textIndex = 1
-local maxTextIndex = 5
+local maxTextIndex = 38
 local tipTexts = {
-     "Find the drop pod supply",
-     "Break out of the orkz base to the Hive City",
-     "Boomboclat",
-     "Rima con el 13",
-     "Benito se apellida camela",
-     "Producer juega mal al padel"
+"Upgrade on the drop pod supply to get stronger!",
+"Chainsword heals for each enemy it hits!",
+"Heal yourself by using a drop pod supply!",
+"Use explosive barrels to damage groups of enemies!",
+"Break scrap piles for lots of resources!",
+"Dash with [B] to escape or gain space!",
+"Swap weapons with [Y] when needed!",
+"Armor upgrades can also change how you look!",
+"Think before rushing into a fight!",
+"Ultramarines follow the Codex better than anyone else.",
+"Guilliman wrote the Codex Astartes to guide the chapters.",
+"Each Ultramarine rank has a symbol in specific armor spots.",
+"Black Templars do not follow the Codex but respect Ultramarines.",
+"Fulgrim wounded Guilliman with a cursed sword.",
+"Orks spread spores and grow from the ground.",
+"Orks were made to fight in an ancient war.",
+"Orks fight all the time, even among themselves.",
+"Gork is brutal but cunning, Mork is cunning but brutal.",
+"Ghazghkull spared Yarrick to keep fighting him later.",
+"Guilliman was revived thanks to Cawl and the Aeldari.",
+"Ultramarines nearly fell during the Battle of Macragge.",
+"Titus resisted Chaos, but the Inquisition still doubted him.",
+"Fulgrim nearly killed Guilliman with a daemon sword.",
+"Ghazghkull is the most feared Ork warboss known.",
+"Ghazghkull fought Yarrick in the War for Armageddon.",
+"In the third war, Ghazghkull returned with a huge WAAAGH!",
+"Ghazghkull wants war, not territory.",
+"Orks existed before humans and fought Necrons and Eldar.",
+"Ultramarines stopped a massive Ork WAAAGH! on Ichar IV.",
+"Let the stars fall, I will free the galaxy or burn it. - Horus",
+"War is hygiene for the galaxy. - Alpharius",
+"I am wrath and death! - Lucius the Eternal",
+"Blessed is the mind too small for doubt. - Mechanicus saying",
+"Only in death does duty end. - Imperial saying",
+"Sanguinius cast demons back to hell with his own hands.",
+"I'm da hand of Gork and Mork, here to wake the boyz! - Ghazghkull",
+"We stomp the galaxy flat, 'cause we was made to fight! - Ghazghkull",
+"What doesn't kill me isn't trying hard enough. - Guilliman",
+"No peace, only carnage and the gods laughter."
 }
 
 
