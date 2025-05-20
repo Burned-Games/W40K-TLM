94f9b6c93419dacd34ce973f8f11af9f0832bba9
Marti Sabaté <71668554+msabate00@users.noreply.github.com>
2025-05-20 16:55:09 +0200

    added tutorial indicator joystick

---
+++ W40K-TLM/scripts/misc/MovementTutorialIndicator.lua
@@ -0,0 +1,89 @@
+local distanceShow = 2.0
+
+
+
+local player  = nil
+local playerTransform  = nil
+local transform = nil
+
+local spriteComponent = nil
+
+local interactionSprite = nil
+local interactionSpriteTransitionTimerTarget = 0.4
+local interactionSpriteTransitionTimer = 0.0
+local outOfRange = true
+local beforeFrameOutOfRange = true
+local hasBeenHidden = false
+
+function on_ready()
+    -- Add initialization code 
+    player = current_scene:get_entity_by_name("Player")
+    playerTransform = player:get_component("TransformComponent")
+    spriteComponent = self:get_component("SpriteComponent")
+    transform = self:get_component("TransformComponent")
+end
+
+function on_update(dt)
+    -- Add update code here
+    beforeFrameOutOfRange = outOfRange
+
+    local distance = Vector3.new(100,100,100)
+    distance = Vector3.new(
+            math.abs(playerTransform.position.x - transform.position.x),
+            math.abs(playerTransform.position.y - transform.position.y),
+            math.abs(playerTransform.position.z - transform.position.z)
+    )
+
+    
+
+    if distance.x < distanceShow and distance.z < distanceShow then
+        if not hasBeenHidden then
+            outOfRange = false
+        end
+        
+    else
+        outOfRange = true
+        hasBeenHidden = true
+    end
+
+    if outOfRange ~= beforeFrameOutOfRange then
+        interactionSpriteTransitionTimer = 0
+    end
+
+    if outOfRange then
+        if spriteComponent then
+            FadeToTransparent(dt)
+            log("fade to transparent")
+        end
+    else 
+        if spriteComponent then
+            FadeToBlack(dt)
+            log("fade to black")
+        end
+    end
+
+
+end
+
+function on_exit()
+    -- Add cleanup code here
+end
+
+function FadeToTransparent(dt)
+    interactionSpriteTransitionTimer = interactionSpriteTransitionTimer + dt
+    local alpha = math.min(interactionSpriteTransitionTimer / interactionSpriteTransitionTimerTarget, 1.0)
+    alpha = 1.0 - alpha -- invertir
+    spriteComponent.tint_color = Vector4.new(1,1,1,alpha)
+    if (interactionSpriteTransitionTimer > interactionSpriteTransitionTimerTarget) then
+        spriteComponent.tint_color = Vector4.new(1,1,1,0)
+    end
+end
+
+function FadeToBlack(dt)
+    interactionSpriteTransitionTimer = interactionSpriteTransitionTimer + dt
+    local alpha = math.min(interactionSpriteTransitionTimer / interactionSpriteTransitionTimerTarget, 1.0)
+    spriteComponent.tint_color = Vector4.new(1,1,1,alpha)
+    if (interactionSpriteTransitionTimer > interactionSpriteTransitionTimerTarget) then
+        spriteComponent.tint_color = Vector4.new(1,1,1,1)
+    end
+end
