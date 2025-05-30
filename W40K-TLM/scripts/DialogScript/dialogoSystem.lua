821d11e88da5a60401b22ea569e710661bf0ff56
Linjiajie2002 <92087408+Linjiajie2002@users.noreply.github.com>
2025-05-30 20:07:31 +0200

    JJL-DialogoAudioDelete

--- W40K-TLM/scripts/DialogScript/dialogoSystem.lua
+++ W40K-TLM/scripts/DialogScript/dialogoSystem.lua
@@ -1,18 +1,6 @@
 
 --Dialogo 2
---Audio
-local dia1_audio1 = nil
-local dia1_audio2 = nil
-local dia1_audio3 = nil
-local dia5L_audio1 = nil
-local dia5S_audio1 = nil
-local dia8_audio1 = nil
-local dia9L_audio1 = nil
-local dia9L_audio2 = nil
-local dia9S_audio1 = nil
-local dia9S_audio2 = nil
-local dia10_audio1 = nil
-local dia11_audio1 = nil
+
 --Dialogo
 local dialogLines1 = nil
 local dialogLines5L = nil
@@ -33,17 +21,6 @@ dialog10 = false
 dialog11 = false
 
 --Dialogo 2
---Audio
-local dia12_audio1 = nil
-local dia13_audio1 = nil
-local dia13Ac_audio1 = nil
-local dia14_audio1 = nil
-local dia15_audio1 = nil
-local dia16_audio1 = nil
-local dia17_audio1 = nil
-local dia18_audio1 = nil
-local dia19_audio1 = nil
-
 
 --Dialogo
 local dialogLines12 = nil
@@ -106,104 +83,82 @@ function on_ready()
     current_scene_tag = self:get_component("TagComponent").tag
 
     if current_scene_name == "level1.TeaScene" then
-        --Audio
-        dia1_audio1 = current_scene:get_entity_by_name("dia1_audio1"):get_component("AudioSourceComponent")
-        dia1_audio2 = current_scene:get_entity_by_name("dia1_audio2"):get_component("AudioSourceComponent")
-        dia1_audio3 = current_scene:get_entity_by_name("dia1_audio3"):get_component("AudioSourceComponent")
 
-        dia5L_audio1 = current_scene:get_entity_by_name("dia5L_audio1"):get_component("AudioSourceComponent")
-        dia5S_audio1 = current_scene:get_entity_by_name("dia5S_audio1"):get_component("AudioSourceComponent")
-        dia8_audio1 = current_scene:get_entity_by_name("dia8_audio1"):get_component("AudioSourceComponent")
-        dia9L_audio1 = current_scene:get_entity_by_name("dia9L_audio1"):get_component("AudioSourceComponent")
-        dia9L_audio2 = current_scene:get_entity_by_name("dia9L_audio2"):get_component("AudioSourceComponent")
-        dia9S_audio1 = current_scene:get_entity_by_name("dia9S_audio1"):get_component("AudioSourceComponent")
-        dia9S_audio2 = current_scene:get_entity_by_name("dia9S_audio2"):get_component("AudioSourceComponent")
-        dia10_audio1 = current_scene:get_entity_by_name("dia10_audio1"):get_component("AudioSourceComponent")
-        dia11_audio1 = current_scene:get_entity_by_name("dia11_audio1"):get_component("AudioSourceComponent")
 
         --Dialogo
         dialogLines1 = {
-            { name = "DeciusMarcellus", text = "This is Decius Marcellus, Commander of Guilliman's Fist. Has anyone successfully made planetfall? Does anyone still live?", audio = dia1_audio1, time =9.6},
-            { name = "DeciusMarcellus", text = "This is Brother Quintus Maxillian, Ultramarine of the 3rd Company. As far as I can tell, I am the only one left.", audio = dia1_audio2, time = 7.5},
-            { name = "DeciusMarcellus", text = "It appears you are the sole survivor. Nonetheless, the mission stands. The Emperor protects, Brother.", audio = dia1_audio3, time = 8}
+            { name = "DeciusMarcellus", text = "This is Decius Marcellus, Commander of Guilliman's Fist. Has anyone successfully made planetfall? Does anyone still live?",  time =9.6},
+            { name = "DeciusMarcellus", text = "This is Brother Quintus Maxillian, Ultramarine of the 3rd Company. As far as I can tell, I am the only one left.", time = 7.5},
+            { name = "DeciusMarcellus", text = "It appears you are the sole survivor. Nonetheless, the mission stands. The Emperor protects, Brother.", time = 8}
         }
         
         dialogLines5L = {
-            { name = "DeciusMarcellus", text = "Brother Maxillian, a supply pod is en route to your position. Use it to upgrade your gear. May the Emperor's light guide your hand.", audio = dia5L_audio1, time = 9}
+            { name = "DeciusMarcellus", text = "Brother Maxillian, a supply pod is en route to your position. Use it to upgrade your gear. May the Emperor's light guide your hand.", time = 9}
         }
 
         dialogLines5S = {
-            { name = "DeciusMarcellus", text = "We're detecting medicae injectors nearby. Tend to your wounds with them before proceeding. The mission must not falter.", audio = dia5S_audio1, time = 7.8}
+            { name = "DeciusMarcellus", text = "We're detecting medicae injectors nearby. Tend to your wounds with them before proceeding. The mission must not falter.", time = 7.8}
         }
 
         dialogLines8 = {
-            { name = "QuintusMaxillian", text = "Brother, the scanner reveals heavy Ork presence. Enter their stronghold and purge them all.", audio = dia8_audio1, time = 6}
+            { name = "QuintusMaxillian", text = "Brother, the scanner reveals heavy Ork presence. Enter their stronghold and purge them all.", time = 6}
         }
 
         dialogLines9L = {
-            { name = "QuintusMaxillian", text = "Commander Decius, I hear Orks nearby. Can you confirm their numbers?", audio = dia9L_audio1, time = 4},
-            { name = "DeciusMarcellus", text = "You are surrounded, Brother. Prepare for a brutal confrontation. The Emperor protects brother.", audio = dia9L_audio2, time = 6}
+            { name = "QuintusMaxillian", text = "Commander Decius, I hear Orks nearby. Can you confirm their numbers?", time = 4},
+            { name = "DeciusMarcellus", text = "You are surrounded, Brother. Prepare for a brutal confrontation. The Emperor protects brother.", time = 6}
         }
 
         dialogLines9S = {
-            { name = "DeciusMarcellus", text = "Status report-are you still with us, Brother?", audio = dia9S_audio1, time = 4},
-            { name = "QuintusMaxillian", text = "I remain unbroken. Still in one piece. Anything ahead I should be wary of?", audio = dia9S_audio2, time = 5}
+            { name = "DeciusMarcellus", text = "Status report-are you still with us, Brother?", time = 4},
+            { name = "QuintusMaxillian", text = "I remain unbroken. Still in one piece. Anything ahead I should be wary of?", time = 5}
         }
 
         dialogLines10 = {
-            { name = "DeciusMarcellus", text = "Nothing more brother, few enemies left. Go ahead brother, clean this place and proceed with the mission.", audio = dia10_audio1, time = 6.5}
+            { name = "DeciusMarcellus", text = "Nothing more brother, few enemies left. Go ahead brother, clean this place and proceed with the mission.", time = 6.5}
         }
 
         dialogLines11 = {
-            { name = "DeciusMarcellus", text = "Little resistance remains. Once you clear the path ahead, proceed directly to Martyria Eterna. Finish this, Brother.", audio = dia11_audio1, time = 8}
+            { name = "DeciusMarcellus", text = "Little resistance remains. Once you clear the path ahead, proceed directly to Martyria Eterna. Finish this, Brother.", time = 8}
         }
     
     elseif current_scene_name == "level2.TeaScene" then
-        --Audio
-        dia12_audio1 = current_scene:get_entity_by_name("dia12_audio1"):get_component("AudioSourceComponent")
-        dia13_audio1 = current_scene:get_entity_by_name("dia13_audio1"):get_component("AudioSourceComponent")
-        dia13Ac_audio1 = current_scene:get_entity_by_name("dia13Ac_audio1"):get_component("AudioSourceComponent")
-        dia14_audio1 = current_scene:get_entity_by_name("dia14_audio1"):get_component("AudioSourceComponent")
-        dia15_audio1 = current_scene:get_entity_by_name("dia15_audio1"):get_component("AudioSourceComponent")
-        dia16_audio1 = current_scene:get_entity_by_name("dia16_audio1"):get_component("AudioSourceComponent")
-        dia17_audio1 = current_scene:get_entity_by_name("dia17_audio1"):get_component("AudioSourceComponent")
-        dia18_audio1 = current_scene:get_entity_by_name("dia18_audio1"):get_component("AudioSourceComponent")
-        dia19_audio1 = current_scene:get_entity_by_name("dia19_audio1"):get_component("AudioSourceComponent")
+
 
 
         --DialogoText
         dialogLines12 = {
-            { name = "DeciusMarcellus", text = "Welcome to Martyria Eterna brother. Find your way into the cathedral and finish Garrosh to end this invasion.", audio = dia12_audio1, time = 7.5}
+            { name = "DeciusMarcellus", text = "Welcome to Martyria Eterna brother. Find your way into the cathedral and finish Garrosh to end this invasion.", time = 7.5}
         }
         
         dialogLines13 = {
-            { name = "DeciusMarcellus", text = "You've reached a sealed sector. Find a manual override, a lever or control panel. Time is not on our side, Brother.", audio = dia13_audio1, time = 8}
+            { name = "DeciusMarcellus", text = "You've reached a sealed sector. Find a manual override, a lever or control panel. Time is not on our side, Brother.", time = 8}
         }
 
         dialogLines13Ac = {
-            { name = "QuintusMaxillian", text = "Lever engaged, moving forward.", audio = dia13Ac_audio1, time = 2}
+            { name = "QuintusMaxillian", text = "Lever engaged, moving forward.", time = 2}
         }
 
         dialogLines14 = {
-            { name = "DeciusMarcellus", text = "Purge all remaining hostiles in the area. Leave no greenskin standing. Martyria Eterna depends on your advance.", audio = dia14_audio1, time = 8.5}
+            { name = "DeciusMarcellus", text = "Purge all remaining hostiles in the area. Leave no greenskin standing. Martyria Eterna depends on your advance.", time = 8.5}
         }
 
         dialogLines15 = {
-            { name = "QuintusMaxillian", text = "I've reached the Central Square of Martyria Eterna. I must explore the area. There has to be a way deeper into the city.", audio = dia15_audio1, time = 7}
+            { name = "QuintusMaxillian", text = "I've reached the Central Square of Martyria Eterna. I must explore the area. There has to be a way deeper into the city.", time = 7}
         }
         
         dialogLines16 = {
-            { name = "DeciusMarcellus", text = "Brother Maxillian, supply pod nearby. Upgrade your gear before advancing. The deeper you go, the deadlier it becomes.", audio = dia16_audio1, time = 9}
+            { name = "DeciusMarcellus", text = "Brother Maxillian, supply pod nearby. Upgrade your gear before advancing. The deeper you go, the deadlier it becomes.", time = 9}
         }
         dialogLines17 = {
-            { name = "DeciusMarcellus", text = "You're approaching the Great Bridge-but the access gate is sealed. Search the area for a lever. Force the passage open.", audio = dia17_audio1, time = 8.5}
+            { name = "DeciusMarcellus", text = "You're approaching the Great Bridge-but the access gate is sealed. Search the area for a lever. Force the passage open.", time = 8.5}
         }
         dialogLines18 = {
-            { name = "DeciusMarcellus", text = "Security protocols have raised the bridge gates. There must be manual overrides nearby. Activate and continue your advance.", audio = dia18_audio1, time = 9}
+            { name = "DeciusMarcellus", text = "Security protocols have raised the bridge gates. There must be manual overrides nearby. Activate and continue your advance.", time = 9}
         }
         
         dialogLines19 = {
-            { name = "DeciusMarcellus", text = "This is it, Brother. Upgrade your gear and tend to your wounds. The final confrontation awaits in the heart of Martyria Eterna.", audio = dia19_audio1, time = 9}
+            { name = "DeciusMarcellus", text = "This is it, Brother. Upgrade your gear and tend to your wounds. The final confrontation awaits in the heart of Martyria Eterna.", time = 9}
         }
        
     elseif current_scene_name == "level3.TeaScene" then
