b27dc3fffc5f0446ea6ee4864467ef68c91be3d1
eduardobr33 <100405421+eduardobr33@users.noreply.github.com>
2025-05-31 11:42:19 +0200

    New scene Boss Arena

--- W40K-TLM/scripts/misc/CinematicIntroBossController.lua
+++ W40K-TLM/scripts/misc/CinematicIntroBossController.lua
@@ -1,23 +1,33 @@
-
-
-local timeToTransition = 2
+local timeToTransition = 0.1
 
 
 
 local contador = 0
 local fadeToBlackScript = nil
 local changeing = false
+local doTransition = false
 
 
 
 
 function on_ready()
-    -- Add initialization code here
     fadeToBlackScript = current_scene:get_entity_by_name("FadeToBlack"):get_component("ScriptComponent")
+
+    triggerBossBattle = self:get_component("RigidbodyComponent")
+    triggerBossBattle.rb:set_trigger(true)
+    
+    triggerBossBattle:on_collision_enter(function(entityA, entityB)
+        local nameA = entityA:get_component("TagComponent").tag
+        local nameB = entityB:get_component("TagComponent").tag
+
+        if nameA == "Player" or nameB == "Player" then
+            doTransition = true
+        end
+    end)
 end
 
 function on_update(dt)
-    -- Add update code here
+    if not doTransition then return end
 
     contador = contador + dt
     if  not changeing and contador > timeToTransition then
@@ -28,7 +38,7 @@ function on_update(dt)
     if changeing and not changeScene then
         if fadeToBlackScript.fadeToBlackDoned then
             changeScene = true
-            SceneManager.change_scene("scenes/level3.TeaScene")
+            SceneManager.change_scene("scenes/bossArena.TeaScene")
         end
     end
 end
