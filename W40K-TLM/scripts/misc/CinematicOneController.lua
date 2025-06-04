dc9fd5fa268fb9b20b55e0db970e3e447ed4b30b
errico6 <125646224+errico6@users.noreply.github.com>
2025-06-04 16:05:04 +0200

    La cienmatica inicial ya no s epuede saltar
    Si se descomenta una cosa en el script se puede volver a saltar

--- W40K-TLM/scripts/misc/CinematicOneController.lua
+++ W40K-TLM/scripts/misc/CinematicOneController.lua
@@ -22,28 +22,34 @@ end
 
 function on_update(dt)
     -- Add update code here
+    --[[
 
 
     if not changeing then
+        
+        
+        
         if Input.get_button(Input.action.Confirm) == Input.state.Repeat then
             uiskipActualOffset = uiskipActualOffset + dt * uiSkipSpeed
             move_ui_element(uiskipEntity, dt * uiSkipSpeed, 0)
         end
+        
 
         if Input.get_button(Input.action.Confirm) == Input.state.Up then
             move_ui_element(uiskipEntity, -uiskipActualOffset, 0)
             uiskipActualOffset = 0
         end
     end
+    ]]
     
-
+    
     contador = contador + dt
     if  not changeing and (contador > timeToTransition or uiskipActualOffset >= 2000) then
         changeing = true
         move_ui_element(uiskipEntity, -2000, 0)
         fadeToBlackScript:DoFade()
     end
-
+    
 
     if changeing and not changeScene then
         if fadeToBlackScript.fadeToBlackDoned then
