a0c53ab45b568691a4f11c84fc7e22960e9283f0
Aleix3 <Aleixbotella.1@gmail.com>
2025-06-05 12:17:59 +0200

    all tutorial completed without bugs

--- W40K-TLM/scripts/player/JugadorScript.lua
+++ W40K-TLM/scripts/player/JugadorScript.lua
@@ -590,7 +590,7 @@ function on_update(dt)
             
     end
     
-    if aimButton:is_active() and isAiming and shootButton:is_active() and currentUpAnim == attack then
+    if aimButton:is_active() and isAiming and shootButton:is_active() and (currentUpAnim == attack or currentUpAnim == shotgun_Pump) then
         aimAndShootTutorialButtonsShown = true
     end
 
@@ -628,6 +628,10 @@ function on_update(dt)
     if hasDoneTutorialChangeWeapon == false and firstEnemyDied and not changeWeaponButton:is_active() and level == 1 and zonePlayer == 0 then
         changeWeaponButton:set_active(true)
         hasDoneTutorialChangeWeapon = true
+    end
+
+    if changeWeaponTutorialButtonShown and not hasDoneTutorialChangeWeapon then
+        changeWeaponTutorialButtonShown = false
     end
 
     if changeWeaponTutorialButtonShown and changeWeaponButton:is_active() and level == 1 and zonePlayer == 0 then
