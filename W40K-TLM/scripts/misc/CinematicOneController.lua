9c8a78353cdc29ddb13426f968f93583b048cbbf
Marti Sabaté <71668554+msabate00@users.noreply.github.com>
2025-06-05 17:36:16 +0200

    cinematic one skip cinemaatic on skip dialogues

--- W40K-TLM/scripts/misc/CinematicOneController.lua
+++ W40K-TLM/scripts/misc/CinematicOneController.lua
@@ -4,28 +4,25 @@ local contador = 0
 local fadeToBlackScript = nil
 local changeing = false
 
-local uiskipEntity = nil
-local uiskipActualOffset = 0
-local uiSkipSpeed = 1000
+
+local dialogManager = nil
 
 
 function on_ready()
     -- Add initialization code here
     fadeToBlackScript = current_scene:get_entity_by_name("FadeToBlack"):get_component("ScriptComponent")
-    uiskipEntity = current_scene:get_entity_by_name("UI_SKIP_Indicator")
+    dialogManager = current_scene:get_entity_by_name("DialogManager"):get_component("ScriptComponent")
     --move_ui_element(uiskipEntity, uiskipActualOffset, -16)
 end
 
 function on_update(dt)
     contador = contador + dt
     Input.send_rumble(0.1, 1, 1) 
-    if  not changeing and (contador > timeToTransition or uiskipActualOffset >= 2000) then
+
+    if  not changeing and (contador > timeToTransition or dialogManager.finishedDialog) then
         changeing = true
-        move_ui_element(uiskipEntity, -2000, 0)
-        
         fadeToBlackScript:DoFade()
     end
-    
 
     if changeing and not changeScene then
         if fadeToBlackScript.fadeToBlackDoned then
