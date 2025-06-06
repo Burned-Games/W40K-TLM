0c5b5b3c0d862e910330732496053a87854a0672
Guillem Alqueza Mayench <guillemalqueza@gmail.com>
2025-06-06 22:51:00 +0200

    shield shader

---
+++ W40K-TLM/shaders/shieldShader.glsl
@@ -0,0 +1,70 @@
+#[vertex]
+
+#version 450 core
+layout (location = 0) in vec3 aPosition;
+layout (location = 1) in vec2 aTexCoord;
+
+layout (std140, binding = 0) uniform camera
+{
+    mat4 projection;
+    mat4 view;
+    vec3 cameraPos;
+};
+
+struct VertexData
+{
+    vec2 TexCoords;
+    vec3 WorldPos;
+    vec3 camPos;
+    vec3 sphereCenter;
+};
+
+layout (location = 2) out VertexData Output;
+
+uniform mat4 model;
+
+void main()
+{
+    Output.TexCoords = aTexCoord;
+    Output.WorldPos = vec3(model * vec4(aPosition, 1.0));
+    Output.camPos = cameraPos;
+    Output.sphereCenter = vec3(model[3]);
+
+    gl_Position = projection * view * vec4(Output.WorldPos, 1.0);
+}
+
+#[fragment]
+
+#version 450 core
+layout(location = 0) out vec4 FragColor;
+layout(location = 1) out vec4 EntityID;
+
+uniform vec3 entityID;
+
+struct VertexData
+{
+    vec2 TexCoords;
+    vec3 WorldPos;
+    vec3 camPos;
+    vec3 sphereCenter;
+};
+
+layout (location = 2) in VertexData VertexInput;
+
+void main()
+{
+    vec3 normal = normalize(VertexInput.WorldPos - VertexInput.sphereCenter);
+
+    vec3 viewDir = normalize(VertexInput.camPos - VertexInput.WorldPos);
+
+    float fresnel = pow(1.0 - max(dot(normal, viewDir), 0.0), 3.0);
+
+    vec3 baseColor = vec3(0.1, 0.2, 1.0);
+    vec3 fresnelColor = vec3(0.3, 1, 0.9);
+    vec3 shieldColor = mix(baseColor, fresnelColor, fresnel);
+
+    float alpha = 0.05 + fresnel * 0.8;
+
+    FragColor = vec4(shieldColor, alpha);
+    EntityID = vec4(entityID, 1.0f);
+}
