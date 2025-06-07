7e48a17e9c57a9cdd5e0296e1c1e00e5da17ab3a
AlejandroIR <shibainuale@gmail.com>
2025-06-07 15:02:09 +0200

    Workbench optimizations (all workbenchs in cache - onready)

--- W40K-TLM/scripts/workbench/WorkBenchUIManager.lua
+++ W40K-TLM/scripts/workbench/WorkBenchUIManager.lua
@@ -6,6 +6,9 @@ local popUpManager = nil
 local pauseMenu = nil
 local chatarraUI = nil
 
+local workbenchEntities = {}
+local workbenchScripts = {}
+
 -- Shared UI elements (General)
 local gBackgroundEntity
 local gDot1ButtonEntity, gDot1Button
@@ -118,6 +121,8 @@ local notAvailableSFX = nil
 local buySFX = nil
 
 function on_ready()
+    initialize_workbench_cache()
+    
     -- Initialize upgrade manager
     upgradeManager = current_scene:get_entity_by_name("UpgradeManager"):get_component("ScriptComponent")
     
@@ -1135,15 +1140,22 @@ function is_workbench_open()
     return isWorkBenchOpen
 end
 
-function find_active_workbench()
-    local workbenchEntities = {}
+function initialize_workbench_cache()
     for i = 1, 10 do
         local workbench = current_scene:get_entity_by_name("Workbench" .. i)
         if workbench then
-            local workbenchScript = workbench:get_component("ScriptComponent")
-            if workbenchScript and workbenchScript.playerInRange then
-                return workbench, workbenchScript
-            end
+            workbenchEntities[i] = workbench
+            workbenchScripts[i] = workbench:get_component("ScriptComponent")
+        end
+    end
+end
+
+function find_active_workbench()
+    for i = 1, 10 do
+        local workbench = workbenchEntities[i]
+        local workbenchScript = workbenchScripts[i]
+        if workbench and workbenchScript and workbenchScript.playerInRange then
+            return workbench, workbenchScript
         end
     end
     return nil, nil
@@ -1166,15 +1178,13 @@ function update_workbench_animation(screen)
     if workbench and workbenchScript and workbenchScript.set_ui_screen then
         workbenchScript:set_ui_screen(screenValue)
     else
-        -- If no active workbench found, try all workbenches that are in ground state
+        -- If no active workbench found, try all cached workbenches that are in ground state
         for i = 1, 10 do
-            local workbench = current_scene:get_entity_by_name("Workbench" .. i)
-            if workbench then
-                local workbenchScript = workbench:get_component("ScriptComponent")
-                if workbenchScript and workbenchScript.workbenchInGround then
-                    workbenchScript:set_ui_screen(screenValue)
-                    break
-                end
+            local workbench = workbenchEntities[i]
+            local workbenchScript = workbenchScripts[i]
+            if workbench and workbenchScript and workbenchScript.workbenchInGround then
+                workbenchScript:set_ui_screen(screenValue)
+                break
             end
         end
     end
