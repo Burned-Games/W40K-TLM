8f460f3d98507ec74184e0278d80faae7834a492
eduardobr33 <100405421+eduardobr33@users.noreply.github.com>
2025-06-07 18:10:28 +0200

    Log cleaned

--- W40K-TLM/scripts/enemies/EnemyBoss.lua
+++ W40K-TLM/scripts/enemies/EnemyBoss.lua
@@ -387,7 +387,6 @@ function main_boss:attack_state()
 
     local distance = main_boss:get_distance(main_boss.enemyTransf.position, main_boss.playerTransf.position)
     local attackChance = math.random()
-    log(main_boss.ultimateScript.ultiTimer)
     if main_boss.isRaging and main_boss.ultimateScript.ultiTimer >= math.random(1, main_boss.ultiCooldown) then
         ultimate_attack()
     else
